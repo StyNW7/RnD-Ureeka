23964fc8d8a17d5a9bf71d451d777554d2b7cc5c
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-10-10 18:04:47 +0700

    Front end development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Background from './pages/background';
 import Hero from './pages/hero';
+import Navbar from './pages/navbar';
+import ThemeToggleButton from '@/components/ui/Toggle';
 
 const HomePage: React.FC = () => {
   return (
     <>
+      <Navbar />
       <Background/>
       <Hero />
+      <ThemeToggleButton />
     </>
   );
 };
